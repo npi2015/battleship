13cc83ad3e73c525863b50681e78435c5d823f00
npi2015 <cuco20011@gmail.com>
2021-06-28 16:58:35 +0200

    Moved barcos class to utils

--- src/constantes.py
+++ src/constantes.py
@@ -1,29 +1,4 @@
 import numpy as np
 
-# Creamos un tablero de 10 * 10
+# Creamos un tablero de 10 * 10 de 0
 tablero = np.zeros([10,10], dtype = np.int32)
-class barcos():
-    def __init__(self):
-        self.portaaviones = 4
-        self.acorazado = 3
-        self.buque = 2
-        self.submarino = 1
-    def colocar_barcos_horizontal(self, row, starting_column):
-        if starting_column + self.portaaviones > 10:
-            print("El barco se sale del tablero")
-        elif not np.all(tablero[row, starting_column: starting_column + self.portaaviones],0):
-            tablero[row, starting_column: starting_column + self.portaaviones] = 1
-        else:
-            print("Ya hay un barco en esa posicion")
-    def colocar_barcos_vertical(self, starting_row, column):
-        if starting_column + self.portaaviones > 10:
-            print("El barco se sale del tablero")
-        elif not np.all(tablero[starting_row: starting_row + self.submarino, column], 0):
-            tablero[starting_row: starting_row + self.submarino, column] = 1
-        else:
-           print("Ya hay un barco en esa posicion")
-
-barcos().colocar_barcos_horizontal(4,8)
-barcos().colocar_barcos_vertical(1,0)
-barcos().colocar_barcos_vertical(1,0)
-print(tablero)
--- src/utils.py
+++ src/utils.py
@@ -4,4 +4,23 @@ class tablero():
     def __init__(self, tablero):
         self.tablero = tablero
 
-    
+class barcos():
+    def __init__(self):
+        self.portaaviones = 4
+        self.acorazado = 3
+        self.buque = 2
+        self.submarino = 1
+    def colocar_barcos_horizontal(self, row, starting_column):
+        if starting_column + self.portaaviones > 10:
+            print("El barco se sale del tablero")
+        elif not np.all(tablero[row, starting_column: starting_column + self.portaaviones],0):
+            tablero[row, starting_column: starting_column + self.portaaviones] = 1
+        else:
+            print("Ya hay un barco en esa posicion")
+    def colocar_barcos_vertical(self, starting_row, column):
+        if starting_column + self.portaaviones > 10:
+            print("El barco se sale del tablero")
+        elif not np.all(tablero[starting_row: starting_row + self.submarino, column], 0):
+            tablero[starting_row: starting_row + self.submarino, column] = 1
+        else:
+           print("Ya hay un barco en esa posicion")
