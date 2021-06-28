2cde1671d20f6d4d5a6054f1746ec01be1de89e5
npi2015 <cuco20011@gmail.com>
2021-06-28 14:26:09 +0200

    testing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-# This is a sample Python script.
+from utils import tablero
+
+tablero = tablero()
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+"File for classes and objects"
+
+class tablero():
+    def __init__(self):
+        print("hola")
